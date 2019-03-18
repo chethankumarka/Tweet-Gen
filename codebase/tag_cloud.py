b695d9e36769fee45775eabcefc7025b3bfc4aea
Shivam Dhar <sdhar3@asu.edu>
2019-03-17 19:40:49 -0700

    adds color to tagclouds

--- codebase/tag_cloud.py
+++ codebase/tag_cloud.py
@@ -4,9 +4,13 @@ from PIL import Image
 import random
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-def grey_color_func(word, font_size, position, orientation, random_state=None,
+def red_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+    return "hsl(0, 100%%, %d%%)" % random.randint(60, 100)
+
+def blue_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(248, 100%%, %d%%)" % random.randint(60, 100)
 
 stopwords = list(STOPWORDS)
 file = open("../dataset/left_tweet_dataset.tsv")
@@ -26,7 +30,7 @@ wordcloud = WordCloud(max_font_size=30, max_words=1000, stopwords=set(stopwords)
 						random_state=1, background_color="black", collocations=False, contour_width=3,
 						contour_color='white').generate(data)
 plt.figure(figsize=[20,10])
-plt.imshow(wordcloud.recolor(color_func=grey_color_func,random_state=3), interpolation='bilinear')
+plt.imshow(wordcloud.recolor(color_func=blue_color_func,random_state=3), interpolation='bilinear')
 plt.axis("off")
+plt.savefig("../images/right.png", format="png")
 plt.show()
-plt.savefig("../images/left.png")
